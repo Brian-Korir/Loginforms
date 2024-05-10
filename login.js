39fc234cfdfdea89ada188549ba70b8ff51cdec9
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-10 22:29:25 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,16 @@
+function showRegister() {
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("register-form").style.display = "block";
+}
+
+function hideRegister() {
+  document.getElementById("login-form").style.display = "block";
+  document.getElementById("register-form").style.display = "none";
+}
+
+function showForgotPassword() {
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("forgot-password-form").style.display = "block";
+}
+
 
